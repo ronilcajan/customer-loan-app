bd2563d171d874ac9b6c40ea823e9096f04333d2
ronilcajan <bugsgamer00@gmail.com>
2019-12-24 11:40:38 +0800

    Update css icons links

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -9,8 +9,8 @@
         <title>Claims - Loan Information System</title>
         <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
         <!--     Fonts and icons     -->
-        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="<? echo base_url();?>assets/iconfont/material-icons.css" />
+        <link rel="stylesheet" href="<? echo base_url();?>assets/css/font-awesome.min.css">
         <!-- CSS Files -->
         <link href="<? echo base_url();?>assets/css/material-dashboard.css?v=2.1.1" rel="stylesheet" />
         <link href="<? echo base_url();?>assets/css/mystyle.css" rel="stylesheet"/> 
