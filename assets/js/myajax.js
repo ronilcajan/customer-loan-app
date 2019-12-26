a76806cd1ef0c7c879b73102aa1fd3ae4a879419
ronilcajan <bugsgamer00@gmail.com>
2019-12-26 22:58:01 +0800

    Add loan function

--- assets/js/myajax.js
+++ assets/js/myajax.js
@@ -177,8 +177,8 @@ $(document).on('click', '.delete', function(){
 }); 
 
 // ============== Account no.query ==============
-$(document).on('blur','.account_no',function(){
-	var account_no = $('.account_no').val();
+$(document).on('blur','.accnt_no',function(){
+	var account_no = $('.accnt_no').val();
 
 	$.ajax({
 		url: 'account-query',
@@ -217,9 +217,114 @@ $(document).on('blur','.account_no',function(){
 				$(".fa-search").removeClass("text-success");
 				$(".acc_no").addClass("has-danger");
 				$(".fa-search").addClass("text-danger");
-				$('.fas').click();
 			}
 		}
+	});
+	return false;
+});
+
+// =========== Insert Loan Details ================
+
+$(document).on('click', '.create-loan', function(e){
+	e.preventDefault();
+
+	var loan_no = $('.loan_no').val();
+	var area = $('.area').val();
+	var account_no = $('.accnt_no').val();
+	var loan_amount = $('.amount').val();
+	var collector = $('.collector').val();
+
+	var email_toggle = $('#email-toggle').hasClass('email');
+	var sim1_toggle = $('#sim1-toggle').hasClass('sim1');
+	var sim2_toggle = $('#sim2-toggle').hasClass('sim2');
+
+	var email_notif = false;
+	var sim1_notif = false;
+	var sim2_notif = false;
+
+	if(email_toggle){
+		var email_notif = true;
+	}
+	if(sim1_toggle){
+		var sim1_notif = true;
+	}
+	if(sim2_toggle){
+		var sim2_notif = true;
+	}
+
+	var b_name = $('.b_name').val();
+	var b_address = $('#c_address').val();
+	
+	if(!b_address){
+		var b_address = 'Purok '+$('.purok_no').val()+','+$('.barangay').val()+','+$('.city').val()+','+$('.province')+','+$('.postal').val(); 
+	}
+
+	var co_maker_name = [];
+	var cedula = [];
+	var date_issued = [];
+	var adrs_issued = [];
+
+	$('.comaker-name').each(function(){
+		co_maker_name.push($(this).val());
+	});
+	$('.cedula').each(function(){
+		cedula.push($(this).val());
+	});
+	$('.date_issued').each(function(){
+		date_issued.push($(this).val());
+	});
+	$('.where_issued').each(function(){
+		adrs_issued.push($(this).val());
+	});
+
+
+	$.ajax({
+		type: "POST",
+		url: "create-loan",
+		dataType: "json",
+		data: {
+			loan_no : loan_no,
+			area : area,
+			account_no : account_no,
+			loan_amount : loan_amount,
+			collector : collector,
+			email_notif : email_notif,
+			sim1_notif : sim1_notif,
+			sim2_notif : sim2_notif,
+			b_name : b_name,
+			b_address : b_address, 
+			co_maker_name : co_maker_name,
+			cedula : cedula,
+			date_issued : date_issued,
+			adrs_issued : adrs_issued
+		},
+		cache: false,
+		beforeSend: function() {
+			$("#loading-screen").show();
+		},
+		success: function(response){
+			if(response.success == true){
+				showNotification(
+					response.messages,
+					"check_circle",
+					"success"
+				);
+				setTimeout(function() {
+							window.location.reload(1);
+				}, 3000);
+				$("#loading-screen").hide();
+			}else{
+
+				$("#loading-screen").hide();
+				
+				showNotification(
+					response.messages,
+					"info",
+					"danger"
+				);
+			}
+		}
+		
 	});
 	return false;
 });
--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -53,7 +53,7 @@ $(document).on('click','#c-add', function() {
 			$('.bs').slideUp('1000', function(){
 				$(this).hide();
 			});
-			
+
 			$('.c-add').slideDown('1000', function(){
 				$(this).show();
 			});
@@ -70,4 +70,33 @@ $(document).on('click','#c-add', function() {
 			$(this).show();
 		});
 	}
+});
+// ========== Adding another co-maker section ===========
+var count = 1;
+$(document).on('click', '#add_co-maker', function(){
+	count += 1;
+
+	var html_code = '';
+	html_code += 	'<div class="row to-remove'+count+'"><div class="col-md-6"><div class="form-group"><label class="bmd-label-floating">Fullname</label>';
+    html_code += 	'<input type="text" id="c-name'+count+'" class="form-control comaker-name" name="comaker-name" required></div></div>';
+    html_code +=	'<div class="col-md-6">';
+    html_code += 	'<button type="button" class="btn btn-social btn-just-icon btn-round btn-danger remove_co-maker" id="'+count+'" rel="tooltip" title="Remove co-maker">';
+    html_code +=    '<i class="fa fa-minus"></i></button></div></div>';
+    html_code += 	'<div class="row to-remove'+count+'"><div class="col-md-4"><div class="form-group"><label class="bmd-label-floating">Residence Certificate No</label>';
+    html_code +=    '<input type="text" class="form-control cedula" name="cedula" id="cedula'+count+'" required></div></div>';                                           
+    html_code +=    '<div class="col-md-4"><div class="form-group"><label class="bmd-label-floating">Date issued</label>';                                                
+    html_code +=    '<input type="date" class="form-control date_issued" name="date_issued" id="c-date'+count+'" required></div></div>'                                                   
+    html_code +=    '<div class="col-md-4"><div class="form-group"><label class="bmd-label-floating">Where issued</label>';                                                  
+    html_code +=	'<input type="text" class="form-control where_issued" name="where_issued" id="c-place'+count+'" required></div></div></div>';                                                       
+ 
+    $('.co-maker-section').append(html_code);
+
+});
+
+//  ============= Removing co-maker section =============
+$(document).on('click', '.remove_co-maker', function(){
+	var div_id = $(this).attr('id');
+	$('.to-remove'+div_id).slideUp('1000', function(){
+    	$(this).remove();
+	});
 });
