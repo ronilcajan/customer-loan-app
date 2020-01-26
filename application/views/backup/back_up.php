c647ae41e01846c7c8b75135ab278bbabbeff0dc
ronilcajan <bugsgamer00@gmail.com>
2020-01-26 22:41:53 +0800

    Edit user page

--- application/views/backup/back_up.php
+++ application/views/backup/back_up.php
@@ -24,9 +24,8 @@
                                     <div class="input-group no-border mt-5">
                                         <div class="ml-auto mr-auto">
 
-                                        <button class="btn btn-primary btn-sm" onclick="location.href='<? echo base_url();?>local-backup'"><i class="fas fa-download"></i> Local</button>
-                                        <a href='<? echo base_url();?>dropbox' class="btn btn-primary btn-sm" onclick="window.open('<? echo base_url();?>dropbox', 'newwindow', 'width=600,height=600'); return false;"><i class="fab fa-dropbox"></i> DropBox</a>
-                                        <a href='<? echo base_url();?>googledrive' class="btn btn-primary btn-sm" onclick="window.open('<? echo base_url();?>googledrive', 'newwindow', 'width=600,height=600'); return false;">Google Drive</a>
+                                        <button class="btn btn-outline-primary btn-lg font-weight-bold" rel="tooltip" title="Download SQL File" onclick="location.href='<? echo base_url();?>local-backup'"><i class="fas fa-download"></i> Local</button>
+                                        <a href='<? echo base_url();?>dropbox' class="btn btn-lg font-weight-bold" style="background-color: #18357D" rel="tooltip" title="Upload SQL File to DropBox " onclick="window.open('<? echo base_url();?>dropbox', 'newwindow', 'width=600,height=600'); return false;"><i class="fab fa-dropbox"></i> DropBox</a>
                                     </div>
                                     </div>
                                 </div>
